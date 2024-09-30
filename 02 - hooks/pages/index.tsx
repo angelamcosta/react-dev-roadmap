a42d3e92869273488a89937028150a9f8e5f2112
Angela Lima <angelamcostalima@icloud.com>
2024-09-30 15:54:06 +0100

    🎨 add more exercises to the `index` page

--- 02 - hooks/pages/index.tsx
+++ 02 - hooks/pages/index.tsx
@@ -2,13 +2,20 @@ import { hoc } from "@/components/Template";
 import { UnorderedList } from "@/components/UnorderedList";
 
 const Template = hoc(UnorderedList);
+const BASIC = "/basic-hooks";
+const COMMON = "/common-hooks";
 
 export default function Index() {
+    const list = [
+        { link: `${BASIC}/usestate`, topic: "useState" },
+        { link: `${BASIC}/useeffect`, topic: "useEffect" },
+        { link: `${COMMON}/useref`, topic: "useRef" },
+    ];
     return (
         <Template
             title='Hooks'
             subtitle='These are exercises for the Hooks section of roadmap.sh for learning React.'
-            list={[{ link: "/basic-hooks/usestate", topic: "useState" }]}
+            list={list}
             goBack={true}
         />
     );
