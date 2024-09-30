e438f61faaee1266bd23857d3cb13184a08c55ca
Angela Lima <angelamcostalima@icloud.com>
2024-09-30 14:52:31 +0100

    ✨ add `basic hooks` exercises

---
+++ 02 - hooks/pages/basic-hooks/useeffect.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { Input } from "@/components/Input";
+import { hoc } from "@/components/Template";
+
+const Template = hoc(Input);
+
+export default function UseEffect() {
+    const [text, setText] = useState("");
+    const [typing, setTyping] = useState(false);
+
+    useEffect(() => {
+        if (text != "") {
+            setTyping(true);
+
+            const timeoutId = setTimeout(() => {
+                setTyping(false);
+            }, 1000);
+
+            return () => clearTimeout(timeoutId);
+        }
+    }, [text]);
+
+    return (
+        <>
+            <p>{text}</p>
+            {typing && <small>User is typing...</small>}
+            <Template
+                type='text'
+                value={text}
+                onChange={(e) => setText(e.currentTarget.value)}
+                title='useEffect'
+                topics='Built-in React Hooks'
+                subtitle={
+                    "These are exercises for the Hooks section of roadmap.sh for learning React."
+                }
+            />
+        </>
+    );
+}
---
+++ 02 - hooks/pages/common-hooks/useref.tsx
@@ -0,0 +1,4 @@
+import { hoc } from "@/components/Template";
+import { useRef } from "react";
+
+
